03bdfa542e4c4ca8ec6e57b2b3908a4e4ade6db7
Sasha Milenkovic <sashamilenkovic2@gmail.com>
2018-07-10 16:25:36 -0500

    omg i hate css

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -5,28 +5,110 @@ import { compose } from 'redux';
 import { USER_ACTIONS } from '../../redux/actions/userActions';
 import { triggerLogout } from '../../redux/actions/loginActions';
 import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
-import ListSubheader from '@material-ui/core/ListSubheader';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
+import Tooltip from '@material-ui/core/Tooltip';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import Typography from '@material-ui/core/Typography';
+import image1 from '../../../src/images/if_Gear_599838.png';
+import image2 from '../../../src/images/if_Settings_2_500624.png';
+import image3 from '../../../src/images/if_Avatar_500621.png';
+
 
 const styles = theme => ({
   root: {
     display: 'flex',
     flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    backgroundColor: theme.palette.background.paper
+    minWidth: 300,
+    width: '100%',
   },
-  gridList: {
-    width: 500,
-    height: 450,
+  image: {
+    position: 'relative',
+    height: 200,
+    [theme.breakpoints.down('xs')]: {
+      width: '100% !important', // Overrides inline-style
+      height: 100,
+    },
+    '&:hover, &$focusVisible': {
+      zIndex: 1,
+      '& $imageBackdrop': {
+        opacity: 0.15,
+      },
+      '& $imageMarked': {
+        opacity: 0,
+      },
+      '& $imageTitle': {
+        border: '4px solid currentColor',
+      },
+    },
   },
-  icon: {
-    color: 'rgba(255, 255, 255, 0.54)',
+  focusVisible: {},
+  imageButton: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: theme.palette.common.white,
+  },
+  imageSrc: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center 40%',
+  },
+  imageBackdrop: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: theme.palette.common.black,
+    opacity: 0.4,
+    transition: theme.transitions.create('opacity'),
+  },
+  imageTitle: {
+    position: 'relative',
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 4}px ${theme.spacing.unit + 6}px`,
+  },
+  imageMarked: {
+    height: 3,
+    width: 18,
+    backgroundColor: theme.palette.common.white,
+    position: 'absolute',
+    bottom: -2,
+    left: 'calc(50% - 9px)',
+    transition: theme.transitions.create('opacity'),
   },
 });
+
+const images = [
+  {
+    url: image1,
+    title: 'Stats',
+    width: '33.3%',
+  },
+  {
+    url: image2,
+    title: 'Preferences',
+    width: '33.3%',
+  },
+  {
+    url: image3,
+    title: 'New Game',
+    width: '33.3%',
+  },
+];
 
 
 
@@ -66,14 +148,46 @@ class UserPage extends Component {
 
           </h1>
           <div className={classes.root}>
-            <GridList cellHeight={180} className = {classes.gridList}>
-              <GridListTile key="Subheader" cols={2} style={{height: 'auto'}}>
-                <ListSubheader component="div">Welcome, { this.props.user.userName }!</ListSubheader>
-              </GridListTile>
-              <GridListTile>
-                <img src={require('../../../src/images/silhouette.png')}/>
-              </GridListTile>
-            </GridList>
+
+
+    return (
+      <div className={classes.root}>
+        {images.map(image => (
+          <ButtonBase
+            focusRipple
+            key={image.title}
+            className={classes.image}
+            focusVisibleClassName={classes.focusVisible}
+            style={{
+              width: image.width,
+            }}
+          >
+            <span
+              className={classes.imageSrc}
+              style={{
+                backgroundImage: `url(${image.url})`,
+              }}
+            />
+            <span className={classes.imageBackdrop} />
+            <span className={classes.imageButton}>
+              <Typography
+                component="span"
+                variant="subheading"
+                color="inherit"
+                className={classes.imageTitle}
+              >
+                {image.title}
+                <span className={classes.imageMarked} />
+              </Typography>
+            </span>
+          </ButtonBase>
+        ))}
+      </div>
+    );
+  }
+
+
+
           </div>
           <button
             onClick={this.logout}
